46ddc2e7e3e1436585cf25b302c470775b55f3f7
Ben Seymour <bseymour@users.noreply.github.com>
2024-10-19 04:50:06 +0100

    London slides, first 2 images

---
+++ app/constants/london-slides.tsx
@@ -0,0 +1,22 @@
+export const londonSlides = [
+  
+  {
+    id: 1,
+    onlyImage: "/london-assets/london-01.png",
+  },
+  {
+    id: 2,
+    onlyImage: "/london-assets/london-02.png",
+  },
+  {
+    id: 1,
+    onlyImage: "/general/generic-slide-1.png",
+  },
+  {
+    id: 2,
+    custom: {
+      content: "Now everybody can cook",
+      image: "/general/slide-2-image.png",
+    },
+  },
+];
--- app/london/page.tsx
+++ app/london/page.tsx
@@ -1,10 +1,10 @@
 import GoogleSlidesCloneComponent from "@/components/google-slides-clone";
-import { genericSlides } from "@/app/constants/slides";
+import { londonSlides } from "@/app/constants/london-slides";
 
 export default function GoogleSlidesClone() {
   return (
     <GoogleSlidesCloneComponent
-      slides={genericSlides}
+      slides={londonSlides}
       initialTitle="London V0 Presentation"
     />
   );
