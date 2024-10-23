eaa5e54fa4751938170e0a4de2c14d18a4f61171
Ben Seymour <bseymour@users.noreply.github.com>
2024-10-23 22:35:18 +0100

    updated slide order for live

--- app/constants/london-slides.tsx
+++ app/constants/london-slides.tsx
@@ -9,9 +9,144 @@ export const londonSlides = [
     onlyImage: "/london-assets/london-02.png",
   },
   {
-    id: 1,
-    onlyImage: "/general/generic-slide-1.png",
+    id: 3,
+    onlyImage: "/london-assets/london-03.png",
   },
+  {
+    id: 4,
+    onlyImage: "/london-assets/london-04.png",
+  },
+  {
+    id: 5,
+    onlyImage: "/london-assets/london-05.png",
+  },
+  {
+    id: 6,
+    onlyImage: "/london-assets/london-06.png",
+  },
+  {
+    id: 7,
+    onlyImage: "/london-assets/london-07.png",
+  },
+  {
+    id: 8,
+    onlyImage: "/london-assets/london-08.png",
+  },
+  {
+    id: 9,
+    onlyImage: "/london-assets/london-09.png",
+  },
+  {
+    id: 10,
+    onlyImage: "/london-assets/london-10.png",
+  },
+  {
+    id: 11,
+    onlyImage: "/london-assets/london-11.png",
+  },
+  {
+    id: 12,
+    onlyImage: "/london-assets/london-12.png",
+  },
+  {
+    id: 13,
+    onlyImage: "/london-assets/london-13.png",
+  },
+  {
+    id: 14,
+    onlyImage: "/london-assets/london-14.png",
+  },
+  {
+    id: 15,
+    onlyImage: "/london-assets/london-15.png",
+  },
+  {
+    id: 16,
+    onlyImage: "/london-assets/london-16.png",
+  },
+  {
+    id: 17,
+    onlyImage: "/london-assets/london-17.png",
+  },
+  {
+    id: 18,
+    onlyImage: "/london-assets/london-18.png",
+  },
+  {
+    id: 19,
+    onlyImage: "/london-assets/london-19.png",
+  },
+  {
+    id: 20,
+    onlyImage: "/london-assets/london-20.png",
+  },
+  {
+    id: 21,
+    onlyImage: "/london-assets/london-21.png",
+  },
+  {
+    id: 22,
+    onlyImage: "/london-assets/london-22.png",
+  },
+  {
+    id: 23,
+    onlyImage: "/london-assets/london-23.png",
+  },
+  {
+    id: 24,
+    onlyImage: "/london-assets/london-24.png",
+  },
+  {
+    id: 25,
+    onlyImage: "/london-assets/london-25.png",
+  },
+  {
+    id: 26,
+    onlyImage: "/london-assets/london-26.png",
+  },
+  {
+    id: 27,
+    onlyImage: "/london-assets/london-27.png",
+  },
+  {
+    id: 28,
+    onlyImage: "/london-assets/london-28.png",
+  },
+  {
+    id: 29,
+    onlyImage: "/london-assets/london-29.png",
+  },
+  {
+    id: 30,
+    onlyImage: "/london-assets/london-30.png",
+  },
+  {
+    id: 31,
+    onlyImage: "/london-assets/london-31.png",
+  },
+  {
+    id: 32,
+    onlyImage: "/london-assets/london-32.png",
+  },
+  {
+    id: 33,
+    onlyImage: "/london-assets/london-33.png",
+  },
+  {
+    id: 34,
+    onlyImage: "/london-assets/london-34.png",
+  },
+  {
+    id: 35,
+    onlyImage: "/london-assets/london-35.png",
+  },
+  {
+    id: 36,
+    onlyImage: "/london-assets/london-36.png",
+  },
+  
+  
+  
   {
     id: 2,
     custom: {
